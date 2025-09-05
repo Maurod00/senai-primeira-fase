989fa706f6d2359433a4f2db4fad5544e8f18684
Maurod00 <mauriciosbd99@gmail.com>
2025-09-04 21:50:16 -0300

    aula html2

---
+++ aulas-portugol/script.js
@@ -0,0 +1,22 @@
+// alert("HTML é legal")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?") 
+
+// let idade 
+
+// idade = 18
+// alert(idade) 
+
+// idade = "melancia"
+// alert (idade)
+
+// idade = 18.1
+// alert(idade) 
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+let amor2 = "meu segundo amor"
+alert(amor2) 
+
