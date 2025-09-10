1fa5add1df6b30488b752daf45484235708f81e0
Maurod00 <mauriciosbd99@gmail.com>
2025-09-09 22:12:14 -0300

    Aula de variáveis no js

---
+++ logica-js/variaveis.js
@@ -0,0 +1,25 @@
+//Variável com var
+// var nome = "Mauricio" // string
+// var nome = "João"
+//  nome = "João"
+
+//Variável com let 
+// let nome = "Mauricio"
+// nome = "João"
+
+//Variável com const 
+const nome = "Mauricio" // string
+const sobrenome = "Rodrigues" 
+const idade = 23 // number 
+const masculino = true // boolean 
+
+//array
+const frutas = ["laranja","maçã", "banana"]
+
+console.log(frutas[0])
+
+
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`) 
+
