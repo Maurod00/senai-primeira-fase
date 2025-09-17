b919c19ae830cdfc084cd70c861468594dcd55b5
Maurod00 <mauriciosbd99@gmail.com>
2025-09-16 21:53:29 -0300

    Aula de lógica em JS

--- logica-js/exercicios-sq2/ex16.js
+++ logica-js/exercicios-sq2/ex16.js
@@ -1,5 +1,6 @@
 // Conversor de Temperatura: Crie um conversor de temperatura que peça ao usuário para inserir uma temperatura em Celsius e, em seguida, converta-a para Fahrenheit. 
 
 let celsius = parseFloat(prompt("Digite a temperatura em Celsius para converter para Fahrenheit"))
-let fahrenheit = prompt(`A conversão é ${(1.8*celsius+32)}`)
+let fahrenheit = (1.8*celsius) + 32
+alert(`A conversão é ${fahrenheit}°F`)
 
