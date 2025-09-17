d035272d2b99ac0a911f3066bd6445c9ce2de526
Maurod00 <mauriciosbd99@gmail.com>
2025-09-16 22:06:53 -0300

    Aula de lógica em JS

--- logica-js/arrays-objetos.js
+++ logica-js/arrays-objetos.js
@@ -10,7 +10,13 @@ const alunosDoSenai = [
       email:"maria@gmail.com", 
       matricula:"321", 
       idade:30 
-    }
+    },
+{
+    nome:"Maurício",
+    email:"mauricio@gmail.com", 
+    matricula: "777",
+    idade:23
+}
 ] 
 
-console.log(alunosDoSenai[1].nome)
+console.log(`Meu nome é ${alunosDoSenai[2].nome} ,meu email é ${alunosDoSenai[2].email} ,minha matricula é ${alunosDoSenai[2].matricula} ,minha idade é ${alunosDoSenai[2].idade}`)
