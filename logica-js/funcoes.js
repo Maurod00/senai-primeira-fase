837484f89c755bf17d40b9a9e17bf68f1bc42648
Maurod00 <mauriciosbd99@gmail.com>
2025-09-30 22:08:51 -0300

    laço

---
+++ logica-js/funcoes.js
@@ -0,0 +1,27 @@
+// function saudacao(nome){
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// saudacao("João")
+// saudacao("Maria") 
+
+// const soma = (x,y)=> x+y // arrow function 
+
+// const soma = (x,y)=>{
+//     return (x+y)
+// }
+
+// console.log(soma(2,4)) 
+
+// const subtracao = (x,y)=> x-y 
+// console.log(subtracao(6,2)) 
+
+// const multi = (x,y)=> x*y 
+// console.log(multi(7,7)) 
+
+const divisao = (x,y)=> x/y
+console.log(divisao(81,9))
