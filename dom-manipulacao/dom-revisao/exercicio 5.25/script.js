a41bd1c51f5ff39085a40556257832b76a3e4931
Maurod00 <mauriciosbd99@gmail.com>
2025-10-27 22:17:17 -0300

    exercicio de Input

---
+++ dom-manipulacao/dom-revisao/exercicio 5.25/script.js
@@ -0,0 +1,28 @@
+let calculo
+let preco 
+let moeda1
+let moeda50
+let moeda25
+let moeda10
+let moeda5
+
+function executarPorco(){
+   preco = parseInt(document.getElementById(inputPreco).value)
+   document.getElementById("inputPreco").value = ""
+   moeda1 = parseInt(document.getElementById(input1real).value)
+   document.getElementById("input1real").value = ""
+   moeda50 = parseInt(document.getElementById(input50cent).value)
+   document.getElementById("input50cent").value = ""
+   moeda25 = parseInt(document.getElementById(input25cent).value)
+   document.getElementById("input25cent").value = ""
+   moeda10 = parseInt(document.getElementById(input10cent).value)
+   document.getElementById("input10cent").value = ""
+   moeda5 = parseInt(document.getElementById(input5cent).value)
+   document.getElementById("input5cent").value = ""
+
+   let calculo = (moeda1+moeda50+moeda25+moeda10+moeda5)
+
+   if (preco < calculo){
+    
+   }
+}
