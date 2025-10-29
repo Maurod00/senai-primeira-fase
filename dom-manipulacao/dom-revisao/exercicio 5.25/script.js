c40305dea29b37ce02893e8bfb2f687aaaa4990f
Maurod00 <mauriciosbd99@gmail.com>
2025-10-29 19:49:47 -0300

    exercicio 5.25

--- dom-manipulacao/dom-revisao/exercicio 5.25/script.js
+++ dom-manipulacao/dom-revisao/exercicio 5.25/script.js
@@ -7,22 +7,26 @@ let moeda10
 let moeda5
 
 function executarPorco(){
-   preco = parseInt(document.getElementById(inputPreco).value)
+   preco = parseInt(document.getElementById("inputPreco").value)
    document.getElementById("inputPreco").value = ""
-   moeda1 = parseInt(document.getElementById(input1real).value)
+   moeda1 = parseInt(document.getElementById("input1real").value)
    document.getElementById("input1real").value = ""
-   moeda50 = parseInt(document.getElementById(input50cent).value)
+   moeda50 = parseInt(document.getElementById("input50cent").value)
    document.getElementById("input50cent").value = ""
-   moeda25 = parseInt(document.getElementById(input25cent).value)
+   moeda25 = parseInt(document.getElementById("input25cent").value)
    document.getElementById("input25cent").value = ""
-   moeda10 = parseInt(document.getElementById(input10cent).value)
+   moeda10 = parseInt(document.getElementById("input10cent").value)
    document.getElementById("input10cent").value = ""
-   moeda5 = parseInt(document.getElementById(input5cent).value)
+   moeda5 = parseInt(document.getElementById("input5cent").value)
    document.getElementById("input5cent").value = ""
 
-   let calculo = (moeda1+moeda50+moeda25+moeda10+moeda5)
-
+   let calculo = (moeda1*1)+(moeda50*0.50)+(moeda25*0.25)+(moeda10*0.10)+(moeda5*0.05);
+   document.getElementById("Resposta").innerHTML = `Total no porco: R$ ${calculo.toFixed(2)}`;
+   
    if (preco < calculo){
+      document.getElementById("Resultado").innerHTML = `Saldo Suficiente para compra`
     
+   } else {
+      document.getElementById("Resultado").innerHTML = `Saldo insuficiente para compra!`
    }
 }
